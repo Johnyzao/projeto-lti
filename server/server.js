9824eacf2bdd2265513c21de0e80e646913a13f0
joaoC <fc58954@alunos.fc.ul.pt>
2024-03-20 09:56:31 +0000

    Implementação da logica de login no route  /login

--- server/server.js
+++ server/server.js
@@ -65,135 +65,151 @@ function validateData(data){
 
     return validacaoDados;
 }
-// TODO:
-// - Verificar se se deve declarar a func de callback do routing como async, porque bcrypt só funciona como asincrono
-// - Verificar qual método dá mais jeito para o switch da query: [Com(devolve >1 tipo de erro) ou Sem(devolve 1 erro apenas)] break;
-// - Interface da página.
 
 app.post("/register", async (req, res) => {
     try{
-
         // ###########################################################
         // #### 1. Validação e limpeza de dados; Hashing da pass. ####
         // ###########################################################
 
+        //Objeto com informação para o front-end
+        const statusMessage = {};
         //Objeto com cópia da receção do pedido
         const parametros = { ...req.body }
 
         //Validação de dados
         const validacaoDados = validateData(parametros);
 
-        //Se todos os dados forem validados
-        if(Object.values(validacaoDados).every(value => value === true)){
-            
-            //Limpeza de caracteres
-            Object.keys(parametros).forEach((key) => {
-                if(typeof parametros[key] === "string"){
-                    parametros[key] = validator.escape(validator.trim(parametros[key]));
-                } 
-            });
+        //Se algum dado falhar a validação
+        if(Object.values(validacaoDados).some(value => value === false)){
+            //console.log(validacaoDados);
+            //const errosDetetados = Object.keys(validacaoDados).filter(key => !validacaoDados[key]);
+            res.status(400).send();
+            return;
+        }
 
-            //Hash da password
-            parametros.pass = await passHash(parametros.pass);
-
-            //Objeto com informação para o front-end
-            const statusMessage = {};
-            
-            console.log(parametros)
+        //Limpeza de caracteres
+        Object.keys(parametros).forEach((key) => {
+            if(typeof parametros[key] === "string"){
+                parametros[key] = validator.escape(validator.trim(parametros[key]));
+            } 
+        });
+        //Hash da password
+        parametros.pass = await passHash(parametros.pass);
+        console.log(parametros)
 
         // #######################################
         // #### 2. Inserção na base de dados. ####
         // #######################################
-            const {nif, nic, nome, gen, dnasc, telemovel, mail, pass} = parametros
-            //Query registo
-            const query = {
-                name: 'register-user',
-                text: 'INSERT INTO utilizador(nif, nic, nome, genero, dnasc, telemovel, email, password) \
-                                VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
-                values: [nif,nic,nome,gen,dnasc,telemovel,mail,pass]
+        const {nif, nic, nome, gen, dnasc, telemovel, mail, pass} = parametros
+        //Query parametrizada que regista um utilizador
+        const queryRegisto = {
+            name: 'register-user',
+            text: 'INSERT INTO utilizador(nif, nic, nome, genero, dnasc, telemovel, email, password) \
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+            values: [nif,nic,nome,gen,dnasc,telemovel,mail,pass]
             } 
-            dbClient.query(query, (error, results) => {
-                        //Caso aconteça um erro na inserção dos dados na BD
-                        if(error){
-                            console.error(error);
-                            switch (error.code){
-                                case '23505': //<UNIQUE> error
-                                    res.status(409);
-                                    const constraintTypes = {
-                                        utilizador_nif_pkey: "userDuplicado",
-                                        unique_mail_constraint: "mailDuplicado",
-                                        unique_nic_constraint: "nicDuplicado",
-                                        unique_phone_constraint: "telemovelDuplicado"
-                                    }
-                                    
-                                    const constraintType = constraintTypes[error.constraint];
-                                    statusMessage[constraintType] = true;
-                                    break;
-
-                                case '23502': //<NOT NULL> error
-                                    res.status(422);
-
-                                    const notNullErrors = {
-                                        nic: 'notNullnic',
-                                        nif: 'notNullnif',
-                                        email: 'notNullmail',
-                                        nome: 'notNullnome',
-                                        dnasc: 'notNulldnasc',
-                                        telemovel: 'notNulltelemovel',
-                                        password: 'notNullpass',
-                                        genero: 'notNullgen'
-                                    };
-
-                                    const errorType = notNullErrors[error.column];
-                                    statusMessage[errorType] = true;
-                                    break;
-
-                                default: //Outros tipos de erros
-                                    res.status(500);
-                                    statusMessage.erroInterno = true;
-                                    break;
-                            }
-
-                            //Devolver a respetiva mensagem de erro para ser tratada no front-end
-                            res.send(statusMessage);
-
-                            } else { //Utilizador inserido com sucesso
-                                res.status(201);
-                                statusMessage.userCriado = true;
-                                res.send(statusMessage);
-                            }                    
-                        });
-        } else { //Se a validação dos dados falhar
-            res.status(400);
-            console.log(validacaoDados);
-            const errosDetetados = Object.keys(validacaoDados).filter(key => !validacaoDados[key]);
-            res.send(errosDetetados);
+            
+        const results = dbClient.query(queryRegisto)
+        
+        //Se não encontrar resultados
+        if(length(results) === 0){
+            //statusMessage.faltaRecurso = true;
+            res.status(404).send();
+            return;
         }
 
-    } catch (error) { //Se o routing falhar
+        //statusMessage.userCriado = true;
+        res.status(201).send();
+
+    } catch (error) { //Se a query ou o servidor falharem
         console.error(error);
-        res.status(404).send("Recurso não encontrado");    
+        switch (error.code){
+            case '23505': //<UNIQUE> error
+                //const constraintTypes = {
+                //    utilizador_nif_pkey: "userDuplicado",
+                //    unique_mail_constraint: "mailDuplicado",
+                //    unique_nic_constraint: "nicDuplicado",
+                //    unique_phone_constraint: "telemovelDuplicado"
+                //}   
+                //onst constraintType = constraintTypes[error.constraint];
+                //statusMessage[constraintType] = true;
+                res.status(409);
+                break;
+
+            case '23502': //<NOT NULL> error
+                //const notNullErrors = {
+                //    nic: 'notNullnic',
+                //    nif: 'notNullnif',
+                //    email: 'notNullmail',
+                //    nome: 'notNullnome',
+                //    dnasc: 'notNulldnasc',
+                //    telemovel: 'notNulltelemovel',
+                //    password: 'notNullpass',
+                //    genero: 'notNullgen'
+                //};
+                //const errorType = notNullErrors[error.column];
+                //statusMessage[errorType] = true;
+                res.status(422);
+                break;
+
+                default: //Outros tipos de erros
+                    res.status(500);
+                    //statusMessage.erroInterno = true;
+                    break;
+                }
+
+        //Devolver a respetiva mensagem de erro para ser tratada no front-end
+        res.send();
+        return;                  
     }
 
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
     try {
         const {mail, pass} = req.body;
         console.log( req.body );
         console.log( "Info de login recebida: Email - " + mail + " Pass - " + pass);
-
-        if ( true ) {
-            res.status(200).send();
-            // Criar session aqui.
-        } else {
-            res.status(401).send(); 
+        
+        //Query parametrizada que procura por um utilizador
+        const queryGetUser = {
+            name: "get-user",
+            text: "SELECT * FROM Utilizador WHERE email = $1",
+            values: [mail]
         }
 
+        //Se não for um mail válido
+        if(!validator.isEmail(mail)){
+            res.status(400).send();
+            return;
+        }
 
+        //Limpeza dos caracteres
+        validator.escape(validator.trim(mail));
+
+        //Query
+        const row = await dbClient.query(queryGetUser);
+
+        //Se não obtiver resultados
+        if(length(row) === 0){
+            res.status(404).send();
+            return;
+        }
+
+        const user = row[0];
+
+        //Compara a pass introduzida com a pass encriptada
+        if (! await bcrypt.compare(pass, user.password)) {
+            res.status(401).send();
+            return;
+        }
+        // Criar session aqui.
+        res.status(201).send();        
     } catch (error) {
         res.status(500).send();
         console.log("Erro no /login " + error);
+        return;
     }
 });
 

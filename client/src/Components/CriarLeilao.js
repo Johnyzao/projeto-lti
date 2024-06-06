663778c0aa50a43bf0a6b2c10614b1b5697ad7ea
heloisaorvalho <126606907+heloisaorvalho@users.noreply.github.com>
2024-06-06 22:52:38 +0100

    Update CriarLeilao.js

--- client/src/Components/CriarLeilao.js
+++ client/src/Components/CriarLeilao.js
@@ -1,21 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link, json } from 'react-router-dom'; // Importando Link
+import { Link } from 'react-router-dom';
 import Header from './Header';
-import Accordion from 'react-bootstrap/Accordion';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button'; // Importando Button do Bootstrap
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import config from '../config';
 
 function CriarLeilao() {
     const [state, setState] = useState({
         startingPrice: 0,
-        selectedObjectId: '', // Campo para armazenar o ID do objeto selecionado
-        success: false
+        selectedObjectId: '',
+        success: false,
+        startDate: '',
+        endDate: ''
     });
 
     const [objetosAchados, setObjetosAchados] = useState([]);
     const [selectedObjectId, setSelectedObjectId] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         axios.get(`${config.LINK_API}/foundObject/user/${JSON.parse(localStorage.getItem("dados")).nif}`)
@@ -46,7 +51,9 @@ function CriarLeilao() {
         e.preventDefault();
         const auctionData = {
             startingPrice: parseFloat(state.startingPrice),
-            selectedObjectId: state.selectedObjectId // Passar o ID do objeto selecionado para o backend
+            selectedObjectId: state.selectedObjectId,
+            startDate: state.startDate,
+            endDate: state.endDate
         };
 
         axios.post(config.LINK_API + '/auction', auctionData)
@@ -54,7 +61,7 @@ function CriarLeilao() {
                 console.log('Novo leilão criado:', response.data);
                 setState({
                     ...state,
-                    success: true // Define sucesso como verdadeiro após a criação do leilão
+                    success: true
                 });
             })
             .catch(error => {
@@ -62,86 +69,122 @@ function CriarLeilao() {
             });
     };
 
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const filteredObjects = objetosAchados.filter(objeto =>
+        objeto.titulo.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const isFormValid = () => {
+        return state.startingPrice !== 0 &&
+            state.selectedObjectId !== '' &&
+            state.startDate !== '' &&
+            state.endDate !== '';
+    };
+
     return (
         <>
             <Header />
-            <div className='item3' style={{ maxWidth: '500px', margin: 'auto', padding: '20px' }}>
+            <Container style={{ maxWidth: '1000px', margin: 'auto', padding: '20px' }}>
                 <h1 style={{ textAlign: 'center' }}>Criar Novo Leilão</h1>
                 <br />
-                <form onSubmit={handleSubmit}>
-                    <Container className='bg-light' fluid='sm'>
-                        <Accordion>
-                            <Accordion.Item eventKey="0">
-                                <Accordion.Header>Objetos Achados registados</Accordion.Header>
-                                <Accordion.Body>
-                                    {objetosAchados.map(objeto => (
-                                        <div key={objeto.id} className="list-group-item d-flex align-items-center">
+                <Form onSubmit={handleSubmit}>
+                    <Row>
+                        <Col md={6} className='bg-light rounded p-4'>
+                            <h5 className='mb-3'>Objetos Não Reclamados</h5>
+                            <Form.Group controlId="formSearch">
+                                <Form.Control 
+                                    type="text" 
+                                    placeholder="Pesquisar objetos..." 
+                                    value={searchTerm} 
+                                    onChange={handleSearchChange} 
+                                    className='mb-3'
+                                />
+                            </Form.Group>
+                            {filteredObjects.length > 0 ? (
+                                <ul className="list-group" style={{ padding: 0 }}>
+                                    {filteredObjects.map(objeto => (
+                                        <li key={objeto.id} className="list-group-item d-flex justify-content-between align-items-center mb-3 p-3 rounded" style={{ border: '1px solid #ddd' }}>
                                             <div>
-                                                <h5>{objeto.titulo}</h5>
-                                                <p>{objeto.descricao}</p>
+                                                <h5 className='mb-0'>{objeto.titulo}</h5>
+                                                <p className='mb-0'>{objeto.descricao}</p>
                                             </div>
-                                            <div className="ml-auto">
-                                                <Button 
-                                                    onClick={() => handleObjectSelect(objeto.id)} 
-                                                    style={{
-                                                        width: '100%', // Garante que os botões tenham o mesmo tamanho
-                                                        background: selectedObjectId === objeto.id ? 'green' : 'blue', 
-                                                        borderColor: selectedObjectId === objeto.id ? 'green' : 'blue'
-                                                    }}
-                                                >
-                                                    {selectedObjectId === objeto.id ? 'Selecionado' : 'Leiloar'}
-                                                </Button>
-                                            </div>
-                                        </div>
+                                            <Button
+                                                onClick={() => handleObjectSelect(objeto.id)}
+                                                className={`btn ${selectedObjectId === objeto.id ? 'btn-success' : 'btn-primary'} px-4 py-2`}
+                                            >
+                                                {selectedObjectId === objeto.id ? 'Selecionado' : 'Selecionar'}
+                                            </Button>
+                                        </li>
                                     ))}
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
-                    </Container>
-                    <br />
-                    <label style={{ display: 'block', marginBottom: '10px' }}>
-                        Preço Inicial:
-                        <input
-                            type="number"
-                            name="startingPrice"
-                            value={state.startingPrice}
-                            onChange={handleInputChange}
-                            step="0.01" // Permitir valores decimais
-                            required
-                            style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
-                        />
-                    </label>
-
-                    <button
-                        type="submit"
-                        style={{
-                            width: '100%',
-                            padding: '10px',
-                            background: 'blue',
-                            color: 'white',
-                            border: 'none',
-                            borderRadius: '5px',
-                            cursor: 'pointer'
-                        }}
-                    >
-                        <Link to="/auction/LeiloesAtivos" style={{ color: 'white', textDecoration: 'none' }}>Criar</Link>
-                    </button>
-                </form>
+                                </ul>
+                            ) : (
+                                <p>Nenhum objeto encontrado.</p>
+                            )}
+                        </Col>
+                        <Col md={1}></Col>
+                        <Col md={5} className='bg-light rounded p-4'>
+                            <label className='mb-3' style={{ width: 'calc(100% - 1.5rem)' }}>
+                                Preço Inicial:
+                                <input
+                                    type="number"
+                                    name="startingPrice"
+                                    value={state.startingPrice}
+                                    onChange={handleInputChange}
+                                    step="0.01"
+                                    required
+                                    className='form-control'
+                                />
+                            </label>
+                            <label className='mb-3' style={{ width: 'calc(100% - 1.5rem)' }}>
+                                Data e Hora de Início:
+                                <input
+                                    type="datetime-local"
+                                    name="startDate"
+                                    value={state.startDate}
+                                    onChange={handleInputChange}
+                                    required
+                                    className='form-control'
+                                />
+                            </label>
+                            <label className='mb-3' style={{ width: 'calc(100% - 1.5rem)' }}>
+                                Data e Hora de Fim:
+                                <input
+                                    type="datetime-local"
+                                    name="endDate"
+                                    value={state.endDate}
+                                    onChange={handleInputChange}
+                                    required
+                                    className='form-control'
+                                />
+                            </label>
+                            <button
+                                type="submit"
+                                className={`btn btn-primary px-4 py-2 mt-3 w-100 ${!isFormValid() && 'disabled'}`}
+                                disabled={!isFormValid()}
+                            >
+                                <Link to="/auction/Leiloes" style={{ color: 'white', textDecoration: 'none' }}>Criar</Link>
+                            </button>
+                        </Col>
+                    </Row>
+                </Form>
                 {state.success && (
-                    <div className="success-message" style={{ marginTop: '20px', textAlign: 'center' }}>
+                    <div className="success-message text-center mt-4">
                         <p>Leilão Criado com Sucesso </p>
-                        <p><Link to="/" style={{ color: 'blue' }}>Clique Aqui</Link> para voltar à página principal.</p>
-                        <p>Ou <Link to="/leiloes">Clique Aqui</Link> para ver os leilões ativos.</p>
+                        <p><Link to="/" className='text-blue'>Clique Aqui</Link> para voltar à página principal.</p>
+                        <p><Link to="/leiloes" className='text-blue'>Clique Aqui</Link> para ver os leilões ativos.</p>
                     </div>
                 )}
-                <div style={{ marginTop: '20px', textAlign: 'center' }}>
+                <div className='text-center mt-4'>
                     {state.selectedObjectId ? (
-                        <span style={{ color: 'green' }}>Objeto Selecionado</span>
+                        <span className='text-success'>Objeto Selecionado</span>
                     ) : (
-                        <span style={{ color: 'red' }}>Nenhum Objeto Selecionado</span>
+                        <span className='text-danger'>Nenhum Objeto Selecionado</span>
                     )}
                 </div>
-            </div>
+            </Container>
         </>
     );
 }
